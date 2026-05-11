954d9dc2e6dd767101033fcbd0887427c77bf7bc
Lander Vohn Namuag <namuag.landervohn@dnsc.edu.ph>
2026-05-12 04:30:00 +0800

    Configure static Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/ts/main.tsx'],
-      refresh: true,
-    }),
-    react(),
-  ],
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
 });
